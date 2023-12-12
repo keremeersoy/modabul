292401e03dc027324e7d7338c8b9e3329a886f52
Kerem Ersoy <azimetkeremersoy@gmail.com>
2023-12-12 23:17:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# modabul app with T3 Tech Stack
+
+This is a [T3 Stack](https://create.t3.gg/) project bootstrapped with `create-t3-app`.
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+
+import { api } from "@/utils/api";
+
+export default function Home() {
+  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>modabul</title>
+        <meta name="description" content="modabul app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        hello world
+      </main>
+    </>
+  );
+}
